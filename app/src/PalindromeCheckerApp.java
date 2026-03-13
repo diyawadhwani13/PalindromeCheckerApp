c81a87a2bfecf7eb0930c7a5077d4e05156b384a
diyawadhwani13 <diyawadhwani13@gmail.com>
2026-03-13 21:42:04 +0530

    Case-Insensitive & Space-Ignored Palindrome

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -2,28 +2,33 @@ import java.util.*;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "madam";
 
-        boolean result = check(input, 0, input.length() - 1);
+            String input = "A man a plan a canal Panama";
 
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + result);
-    }
 
-    // Recursive method
-    private static boolean check(String s, int start, int end) {
+            input = input.replaceAll("\\s+", "").toLowerCase();
 
-        // Base condition
-        if (start >= end) {
-            return true;
+            boolean result = check(input, 0, input.length() - 1);
+
+            System.out.println("Input : " + input);
+            System.out.println("Is Palindrome? : " + result);
         }
 
-        // If mismatch found
-        if (s.charAt(start) != s.charAt(end)) {
-            return false;
-        }
 
-        // Recursive call
-        return check(s, start + 1, end - 1);
+        private static boolean check(String s, int start, int end) {
+
+
+            if (start >= end) {
+                return true;
+            }
+
+
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+
+
+            return check(s, start + 1, end - 1);
+        }
     }
-}
+
