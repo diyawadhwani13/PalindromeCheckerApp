f83cb15e2d8a58882e44f5f1537d60e0c595c157
diyawadhwani13 <diyawadhwani13@gmail.com>
2026-03-16 09:11:49 +0530

    ObjectOrientedPalindromeService

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,34 +1,37 @@
 import java.util.*;
 
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-
-            String input = "A man a plan a canal Panama";
 
 
+    static class PalindromeChecker {
+
+        public boolean checkPalindrome(String input) {
             input = input.replaceAll("\\s+", "").toLowerCase();
-
-            boolean result = check(input, 0, input.length() - 1);
-
-            System.out.println("Input : " + input);
-            System.out.println("Is Palindrome? : " + result);
+            return check(input, 0, input.length() - 1);
         }
 
-
-        private static boolean check(String s, int start, int end) {
-
+        private boolean check(String s, int start, int end) {
 
             if (start >= end) {
                 return true;
             }
 
-
             if (s.charAt(start) != s.charAt(end)) {
                 return false;
             }
-
 
             return check(s, start + 1, end - 1);
         }
     }
 
+    public static void main(String[] args) {
+
+        String input = "racecar";
+
+        PalindromeChecker checker = new PalindromeChecker();
+        boolean result = checker.checkPalindrome(input);
+
+        System.out.println("input: " + input);
+        System.out.println("Is Palindrome? : " + result);
+    }
+}
